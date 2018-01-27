f33ad856ab10a4a5813e222227832c4f78e66c38
moonbench <34589537+moonbench@users.noreply.github.com>
2018-01-27 15:36:09 -0800

    Improve default params for entities/sprites
    require less upfront, make use of the defaults

--- src/entities/sprite.js
+++ src/entities/sprite.js
@@ -1,13 +1,13 @@
 "use strict";
 
 class Sprite extends Entity {
-  constructor(x, y, width, height, angle, mass, asset, repeat){
+  constructor(asset, x, y, width, height, angle, mass){
     super(x, y, width, height, angle, mass);
     this.asset = asset;
-    this.repeat = repeat;
+    this.repeat = false;
   }
 
-  render(ctx, dt){
+  render(ctx){
     if(this.asset)
       if(this.repeat){
         ctx.fillStyle = ctx.createPattern(this.asset, this.repeat);
@@ -19,6 +19,6 @@ class Sprite extends Entity {
       ctx.fillStyle = "#222222";
       ctx.fillRect(-this.height/2, -this.width/2, this.height, this.width);
     }
-    super.render(ctx, dt);
+    super.render(ctx);
   }
 }
